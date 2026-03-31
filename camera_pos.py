e8214370b7c857a5a0a189f08f6d99b9d17e333f
rhaven222 <reesehaven@gmail.com>
2026-03-31 15:51:35 -0400

    test

--- camera_pos.py
+++ camera_pos.py
@@ -6,8 +6,18 @@ camera = robot.camera
 
 print("Testing camera movement")
 
+print("Centering camera")
 camera.center()
+time.sleep(2)
+print("Looking left")
 camera.look_left()
+time.sleep(2)
+print("Looking right")
 camera.look_right()
+time.sleep(2)
+print("Looking Up")
 camera.look_up()
+time.sleep(2)
+print("Looking Forward")
 camera.look_forward()
+time.sleep(2)
