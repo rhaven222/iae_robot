6d0639e6abd0192be428fe126646b175df091c68
rhaven222 <reesehaven@gmail.com>
2026-03-31 14:50:19 -0400

    add camera servos

--- simple_test.py
+++ simple_test.py
@@ -4,17 +4,34 @@ import time
 robot = Robot()
 motors = robot.motors
 arm = robot.arm
+camera = robot.camera
 
 arm.center()
+camera.center()
 time.sleep(2)
+
 motors.move_forward(3, 0.5)
+
+camera.look_left(0.02)
+time.sleep(1)
+camera.look_right(0.02)
+time.sleep(1)
+camera.center(0.02)
 
 arm.raise_arm(0.02)
 arm.open_claw(0.02)
 
-motors.turn_right(3, 0.3) #180
+camera.look_up(0.02)
+time.sleep(1)
+
+motors.turn_right(3, 0.3)  # 180
+
+camera.look_down(0.02)
+time.sleep(1)
 
 arm.fold_arm(0.02)
 arm.close_claw(0.02)
+
+camera.center(0.02)
 
 motors.move_forward(3, 0.5)
