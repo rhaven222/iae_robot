60a96ed664ec321e75d165fd88f1746cbc976bae
rhaven222 <reesehaven@gmail.com>
2026-03-31 15:30:44 -0400

    corrections

--- camera_pos.py
+++ camera_pos.py
@@ -6,22 +6,7 @@ camera = robot.camera
 
 print("Testing camera movement")
 
-print("Centering camera")
-camera.center()
+camera.set_pan(90)
 time.sleep(2)
 
-print("Looking left")
-camera.look_left(0.02) 
-time.sleep(2)
-
-print("returning to center")
-camera.center(0.02)
-time.sleep(2)
-
-print("Looking right")
-camera.look_right(0.02)
-time.sleep(2)
-
-print("returning to center")
-camera.center(0.02)
-time.sleep(2)
+camera.set_tilt(90)
