9eeb80b5605db14b0379327628a595ef1a833526
rhaven222 <reesehaven@gmail.com>
2026-04-06 13:51:14 -0400

    test

--- Test_Scripts/sonar.py
+++ Test_Scripts/sonar.py
@@ -2,7 +2,7 @@ from gpiozero import DistanceSensor
 from time import sleep
 
 # echo=24, trigger=25
-sensor = DistanceSensor(echo=24, trigger=25)
+sensor = DistanceSensor(echo=18, trigger=23)
 
 while True:
     distance = sensor.distance * 100  # convert to cm
