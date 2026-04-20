e643b78e5be3ebc4324353c3d996db5059400107
rhaven222 <reesehaven@gmail.com>
2026-04-20 14:49:13 -0400

    fix

--- Test_Scripts/sonar.py
+++ Test_Scripts/sonar.py
@@ -1,7 +1,7 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(echo=18, trigger=23, max_distance=2)
+sensor = DistanceSensor(echo=23, trigger=18, max_distance=2)
 
 while True:
     print("meters:", sensor.distance)
