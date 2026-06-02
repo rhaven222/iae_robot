a0ab33add6515ba4fcf5f03d8193456a0763efc7
rhaven222 <reesehaven@gmail.com>
2026-06-02 11:09:11 -0400

    test

--- tank_robot/sensor_streaming/gyro_test.py
+++ tank_robot/sensor_streaming/gyro_test.py
@@ -1,27 +1,6 @@
-import sys
-import time
-sys.path.append("/home/megan/iae_robot/tank_robot/sensor_streaming")
-from common.functions import *
-from common.sensors import Gyro
+from mpu6050 import mpu6050
 
+sensor = mpu6050(0x68)
 
-robot = Robot()
-gyro = Gyro()
-gyro.calibrate()
-
-while True:
-    data = gyro.get_sensor_data()
-
-    print(
-        f"Heading: {data['heading']:.2f} deg | "
-        f"Gyro Z: {data['gyro']['z']:.2f} | "
-        f"Accel X: {data['accel']['x']:.2f} "
-        f"Y: {data['accel']['y']:.2f} "
-        f"Z: {data['accel']['z']:.2f}"
-    )
-    robot.turn_angle_gyro(90)
-
-    robot.drive_straight_gyro(speed=0.3, duration=5)
-
-    robot.turn_angle_gyro(-90)
-    time.sleep(0.1)
+print(sensor.get_gyro_data())
+print(sensor.get_accel_data())
